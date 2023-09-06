e77de2852404e346ce54efa8b563b7412c2cf282
AlphaInventory <grupoalpha.infotep@gmail.com>
2023-09-06 17:41:24 -0400

    Confirmacion de envios de datos y modales

--- GrupoAlpha/static/JS/ventanaModal.js
+++ GrupoAlpha/static/JS/ventanaModal.js
@@ -81,24 +81,3 @@ window.addEventListener('click', function(e){
         modalMM.style.display = 'block';
     }
 });
-
-// MODAL ELIMINAR CUENTA
-let modalEC = document.getElementById('miModalEC');
-let flexEC = document.getElementById('flexEC');
-let abrirEC = document.getElementById('abrirEC');
-let cerrarEC = document.getElementById('closeEC');
-
-abrirEC.addEventListener('click', function () {
-    modalEC.style.display = 'block';
-});
-
-cerrarEC.addEventListener('click', function () {
-    modalEC.style.display = 'none';
-});
-
-window.addEventListener('click', function (e) {
-    console.log(e.target);
-    if (e.target == flexEC) {
-        modalEC.style.display = 'block';
-    }
-});
